fe66fcf7b07909a37e21e09fb15b375bfd808aa4
nikita-sinha <nikita.sinha@talentica.com>
2018-04-06 11:55:16 +0530

    My Classes Screen added and listed in drawer menu

--- app/PragatApp/src/app/navigation/DrawerNavigator.tsx
+++ app/PragatApp/src/app/navigation/DrawerNavigator.tsx
@@ -1,13 +1,15 @@
 import { StackNavigator, DrawerNavigator } from "react-navigation";
 
 import MyPlanScreen from "./../screens/MyPlanScreen";
+import MyClassesScreen from "./../screens/MyClassesScreen";
 import DashBoardScreen from "./../screens/DashBoardScreen";
 import DrawerContent from "./DrawerContent";
 
 export const DrawerStack = DrawerNavigator(
   {
     dashBoard: { screen: DashBoardScreen },
-    myPlan: { screen: MyPlanScreen }
+    myPlan: { screen: MyPlanScreen },
+    myClasses: { screen: MyClassesScreen }
   },
   {
     drawerPosition: "right",
---
+++ app/PragatApp/src/app/screens/MyClassesScreen.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+import { View, Text, Image, StyleSheet } from "react-native";
+
+interface props {}
+
+interface state {}
+
+export default class MyClassesScreen extends React.Component {
+  static navigationOptions = {
+    title: "My Classes"
+  };
+  
+  render() {
+    return (
+      <View style={styles.container}>
+          <Text style={{ fontSize: 40, color: "white" }}>My Classes</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#2a8bcc",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  icon: {
+    width: 24,
+    height: 24
+  }
+});
